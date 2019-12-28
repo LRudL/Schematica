a3f43a778e020d0d1e5c542b595c82dfb0f30e3e
CharCoding <st-011510@aas.ru>
2019-12-28 12:02:13 -0500

    fix parse() and validate() bugs, add comments

--- lisk2.js
+++ lisk2.js
@@ -1,77 +1,97 @@
 'use strict';
-const stackLimit = 1 << 10, loopLimit = 1 << 15, epsilon = 1e-10,
-  token = /('?\(|\)|"(\\?[^\\"]|\\\\|\\")*"|[^()\s"]+)/g;
-const time = (fn, args) => {
+/* Note:
+I'm only committing this file right now so that you don't need to resolve merge conflicts.
+I actually already removed the jquery dependency on my branch, which didn't cause any problems.
+parse(libraryCode) is *only* twice as fast as running parseCode(libraryCode) on Firefox, and it's not
+producing the exact output yet, but I think it will do better in the long run. Also, just running
+the regex took about half the time.
+*/
+const stackLimit = 1 << 10, loopLimit = 1 << 15, epsilon = 1e-10, // epsilon is the true error term. STATS/ECON KNOWLEDGE
+  stringToken = /"(\\?[^\\"]|\\\\|\\")*"/g, // magic
+  token = /('?\(|\)|"(\\?[^\\"]|\\\\|\\")*"|[^()\s"]+)/g, // more magic
+  ccss = ["color:white;background-color:red;","color:unset;background-color:unset;","color:yellow"]; // console can be colorful! see validate()
+const time = (fn, args) => { // record function time
   console.time(fn.name);
   const res = fn(args);
   console.timeEnd(fn.name);
   return res;
-}, validate = str => {
-  let openClose = 0, lineNum = 1, colNum = 1;
+}, validate = str => { // checks if the lisk code has no obvious syntax errors, returns true if none found and false otherwise.
+  str = str.replace(stringToken, s => s.replace(/"/g, '|').replace(/\(/g, '[').replace(/\)/g, ']')); // replace string literals with placeholders
+  let openClose = 0, line = 1, column = 1; // line/column number is more helpful than index
   const openPos = [];
   for(let i = 0, l = str.length; i < l; i++) {
-    if(str[i] == '\n') {
-      ++lineNum;
-      colNum = 0;
-    } else if(str[i] == '(') {
-      ++openClose;
-      openPos.push([lineNum, colNum]);
-    } else if(str[i] == ')') {
-      if(--openClose < 0) {
-        console.log(`Unmatched ')' on line ${lineNum}, column ${colNum}.`);
+    switch(str[i]) {
+      case '\n': // newline: increase line and reset column
+        ++line;
+        column = 0;
+        break;
+      case '(':
+        ++openClose;
+        openPos.push([line, column]); // keep track of every ('s position in case they're unmatched.
+        break;
+      case ')':
+        if(--openClose < 0) { // open - close can't be negative
+          console.log(`Unmatched %c)%c on line %c${line}%c, column %c${column}%c.`, ccss[0], ccss[1], ccss[2], ccss[1], ccss[2], ccss[1]);
+          return false; // %c is a css styling directive in console, which get replaced by the corresponding css rules.
+        }
+        openPos.pop();
+        break;
+      case '\'': // can't follow a ' with any whitespace, ), end of script, or another '
+        if(str[i + 1] === undefined || /[\s')]/.test(str[i + 1])) {
+          console.log(`%c'%c requires a valid operand on line %c${line}%c, column %c${column}%c.`, ccss[0], ccss[1], ccss[2], ccss[1], ccss[2], ccss[1]);
+          return false;
+        }
+        break;
+      case '"': // my regex *should* always work, so any leftover " is unmatched... I think...
+        console.log(`Unmatched %c"%c on line %c${line}%c, column %c${column}%c.`, ccss[0], ccss[1], ccss[2], ccss[1], ccss[2], ccss[1]);
         return false;
-      }
-      openPos.pop();
     }
-    ++colNum;
+    ++column;
   }
-  if(openClose) {
-    [lineNum, colNum] = openPos[openPos.length - openClose];
-    console.log(`Unmatched '(' on line ${lineNum}, column ${colNum}`);
+  if(openClose) { // there are still unclosed (, look back at first unmatched
+    [line, column] = openPos[openPos.length - openClose];
+    console.log(`Unmatched %c(%c on line %c${line}%c, column %c${column}%c.`, ccss[0], ccss[1], ccss[2], ccss[1], ccss[2], ccss[1]);
     return false;
   }
   return true;
-}, parse = str => {
-  const matches = str.match(token), program = [], path = [];
-  let quoteDepth = 0, temp = program;
+}, parse = str => { // This algorithm parses nested lisk expressions WITHOUT USING RECURSION (REEEEE)
+  const matches = str.match(token), program = [], path = []; // break down str into tokens; keep everything in an array;
+  // path gives the last position in the program where the last insertion happened.
+  // it tells you where to insert the next element and how deep into the nested array we are.
+  // i.e. if program = [1, [2, [3], 4], 5] and path = [1, 1], the next insertion will go in the same array as 3.
+  let quoteDepth = 0, temp = program; // quoteDepth deals with converting '() to (quote ()), only used on lists and not symbols
+  // temp keeps up with the current insertion array within program
   for(let i = 0, l = matches.length; i < l; i++) {
     let match = matches[i];
-    if(match == '\'(') {
-      let temp = program;
-      for(let i = 0; i < path.length; i++)
-        temp = temp[path[i]];
-      temp.push(["quote", []]);
-      path.push(temp.length - 1, 1);
-      ++quoteDepth;
-    } else if(match[0] == '\'') {
-      let temp = program;
-      for(let i = 0; i < path.length; i++)
-        temp = temp[path[i]];
-      temp.push(["quote", match.slice(1)]);
-    } else if(match == '(') {
-      let temp = program;
-      for(let i = 0; i < path.length; i++)
-        temp = temp[path[i]];
-      temp.push([]);
-      path.push(temp.length - 1);
-      if(quoteDepth)
-        ++quoteDepth;
-    } else if(match == ')') {
-      path.pop();
-      if(quoteDepth)
-        if(!--quoteDepth)
+    if(match == '\'(') { // encountering a quoted list
+      temp.push(["quote", []]); // push (quote ())
+      path.push(temp.length - 1, 1); // update path: new path is the inner ()
+      temp = temp[temp.length - 1][1]; // temp is now the ()
+      ++quoteDepth; // quoteDepth is nonzero; it now counts when it exits the quoted list
+    } else if(match[0] == '\'') { // encountering a symbol
+      temp.push(["quote", match.slice(1)]); // just push the quoted symbol
+    } else if(match == '(') { // encountering a list
+      temp.push([]); // push ()
+      path.push(temp.length - 1); // update path: new path is the () that was just pushed
+      temp = temp[temp.length - 1]; // temp is now the () to insert into
+      if(quoteDepth) // if we're already in quote mode, increase quoteDepth.
+        ++quoteDepth; // so that we don't accidently leave quote mode too early
+    } else if(match == ')') { // leaving a list
+      path.pop(); // remove deepest path
+      if(quoteDepth) // if we're in quote mode, rise one level out of it
+        if(!--quoteDepth) // if that's the last level (quoteDepth = 0), we've exited quote mode. pop again to leave the (quote) layer.
           path.pop();
-    } else {
-      if(!isNaN(+match))
+      temp = program; // we need to upate temp, because it's no longer at the relevant layer.
+      for(let i = 0; i < path.length; i++) // since we can't find its parent list, just find it from program via the path
+        temp = temp[path[i]]; // this works regardless of whether we were just exiting quote mode
+    } else { // encountering some token.
+      if(!isNaN(+match)) // convert numerals to number if possible
         match = +match;
-      let temp = program;
-      for(let i = 0; i < path.length; i++)
-        temp = temp[path[i]];
-      temp.push(match);
+      temp.push(match); // push self-evaluating token
     }
   }
-  return program;
-}, execute = (...arr) => { // non-nested expressions
+  return program; // there, a nested array.
+}, execute = (...arr) => { // for non-nested expressions. This has not been used/tested.
   arr.forEach((e, i, a) => {
     if(e[0] == '\'')
       a[i] = this[e.slice(1)];
@@ -97,7 +117,7 @@ const time = (fn, args) => {
     case '/':
       return arr.reduce((a, b) => a / b);
     case '>':
-      return arr.shift() > Math.max(...arr);
+      return arr.shift() > Math.max(...arr); // UNTESTED, MIGHT BE BUGGY
     case '<':
       return arr.shift() < Math.min(...arr);
     case '>=':
@@ -108,10 +128,10 @@ const time = (fn, args) => {
       return arr[0] - Math.floor(arr[0] / arr[1]) * arr[1];
     case 'ln':
       return Math.log(arr[0]);
-    case 'log':
-      return Math.log(arr[1]) / Math.log(arr[0]); // (log a b) => log_a (b)
+    case 'log': // I like this design better: ln is log, log is log with any base
+      return Math.log(arr[1]) / Math.log(arr[0] || Math.E); // (log a b) => log_a (b)
     case 'let':
-      this[arr[0]] = arr[1];
+      this[arr[0]] = arr[1]; // haven't really thought about how to implement variable scope/environment
       return;
     case 'quote':
       return arr;
@@ -122,4 +142,41 @@ const time = (fn, args) => {
         return Math[fn](...arr);
 
   }
-}
+}, test = () => {
+  const strs = [
+    '(a \'(0 (1 2)) b) (c d (e) \'f)',
+    `(let bound-lines
+     (map (! (pair)
+             (let p0 (nth pair 0))
+             (let p1 (nth pair 1))
+             (cline-from-points
+              (x-of p0) (y-of p0)
+              (x-of p1) (y-of p1)))
+          (pair-adjacents
+           (append canvas-corners
+                   (first canvas-corners)))))`,
+    `(def (list-merge a b op)
+  (def (merger a b op res)
+    (if (= (length a) 0)
+      res
+      (merger (rest a) (rest b) op
+        (concat res (list (op (first a) (first b)))))))
+  (merger a b op '()))
+
+(def (pair-adjacents l)
+  (def (pairer l n res)
+    (if (= n (length l))
+      res
+      (pairer l (++ n)
+              (append res
+                      (list (nth l (-- n))
+                            (nth l n))))))
+  (pairer l 1 '()))`
+  ];
+  for(let i = 3; i--;)
+    console.log(validate(strs[i]));
+  for(let i = 3; i--;)
+    console.log(parse(strs[i]).toSource());
+};
+test();
+// console.log(time(parse,libraryCode).toSource()); // should parse the standard library in ~8ms
